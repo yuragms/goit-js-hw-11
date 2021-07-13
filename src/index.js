8d3842407cba750abc1d64dcc96b1f77aa2a84b3
yuragms <yura.gms@gmail.com>
2021-07-13 09:08:31 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './sass/main.scss';
 
+// <!--Задание 1 - переключатель цветов -->
+
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const tagBody = document.querySelector('body');
@@ -38,3 +40,24 @@ function removeBgcolor (e) {
     tagBody.setAttribute("style", `background-color: ${randomColor};`);
   }
 
+  // <!--Задание 2 - таймер обратного отсчета -->
+
+  const inputData = document.querySelector('#date-selector');
+  const btnStart1 = document.querySelector('[data-start1]');
+  
+  inputData.addEventListener('input', inputValidator);
+
+  const a = new Date();
+
+  const currentDay = a.toDateString();
+
+  function inputValidator () {
+      console.log(inputData.value);
+      const b = inputData.value;
+      const c = b.toDateString();
+      console.log(Date.parse(c));
+      console.log(currentDay);
+      console.log(Date.parse(currentDay));
+  }
+
+  
